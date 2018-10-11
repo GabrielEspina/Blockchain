845fccb4da6af8563422e77421c076291d24d7e5
Fernando Romero <fer.romero1991@gmail.com>
2018-10-11 17:42:04 -0300

    Se modifica los metodos para agregar configuracion

--- src/ar/edu/ub/si/blockchain/almacen/AlmacenArchivo.java
+++ src/ar/edu/ub/si/blockchain/almacen/AlmacenArchivo.java
@@ -4,17 +4,20 @@ import java.util.ArrayList;
 
 import ar.edu.ub.si.blockchain.data.Bloque;
 import ar.edu.ub.si.blockchain.interfaces.IAlmacenBlockchain;
+import ar.edu.ub.si.blockchain.util.Configuracion;
 
 public class AlmacenArchivo implements IAlmacenBlockchain{
 
 	@Override
-	public ArrayList<Bloque> getBlockChain() {
+	public ArrayList<Bloque> getBlockChain(Configuracion configuracion) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+
+
 	@Override
-	public void saveBlockChain(ArrayList<Bloque> blockChain) {
+	public void saveBlockChain(Bloque blockChain, Configuracion configuracion) throws Exception {
 		// TODO Auto-generated method stub
 		
 	}
