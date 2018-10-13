8c9383cd838da446b14d36048de0726dd9333335
Fernando Romero <fer.romero1991@gmail.com>
2018-10-12 22:30:20 -0300

    fixs

--- src/ar/edu/ub/si/blockchain/vista/MainScreen.java
+++ src/ar/edu/ub/si/blockchain/vista/MainScreen.java
@@ -1,6 +1,7 @@
 package ar.edu.ub.si.blockchain.vista;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -16,11 +17,6 @@ public class MainScreen extends JFrame
 
 	private static final long serialVersionUID = 1L;
 	
-	private JPanel jPanel;
-	private JLabel texto;
-	private JButton boton;
-	private JFileChooser selectorArchivo;
-	private File archivo;
 	private String ruta;
 	
 	private IAdministradorBlockchain admin;
@@ -29,13 +25,12 @@ public class MainScreen extends JFrame
 		setAdmin(admin);
 		this.startWindow();
 		this.addElements();
-		//setActionListeners();		
 	}
 
 	private void startWindow() {
 		this.setLocation(300, 300);
-		this.setSize(400, 400);
-		this.setResizable(false);
+		this.setSize(100, 100);
+		this.setResizable(true);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);	
 	}
 
@@ -51,26 +46,6 @@ public class MainScreen extends JFrame
 		
 		this.setJMenuBar(menuBar);
 		
-		/*
-		jPanel = new JPanel();
-		
-		texto = new JLabel();
-		boton = new JButton();
-		
-		selectorArchivo = new JFileChooser();
-		selectorArchivo.setFileSelectionMode(0);
-        FileNameExtensionFilter extensionFilter = new FileNameExtensionFilter("PDF", "pdf");
-        selectorArchivo.setFileFilter(extensionFilter);
-        
-        selectorArchivo.setDialogTitle("Abrir Archivo");
-        
-		this.add(jPanel);
-		jPanel.setBackground(Color.white);
-		jPanel.add(texto);
-		jPanel.add(boton);
-		
-		boton.setText("Buscar Archivo");
-		*/
 	}
 
 	private JMenu createMenuBd() {
@@ -113,20 +88,22 @@ public class MainScreen extends JFrame
 		JFileChooser file = new JFileChooser();
 		file.showOpenDialog(this);
 		File archivo = file.getSelectedFile(); 
+		String texto;
 		
-		String texto = archivo.getName();
-		
-		ruta = file.getSelectedFile().toString();
 		
 		try {
 			if(archivo.exists()) {
+				ruta = file.getSelectedFile().toString();
                 System.out.println(ruta);
+                texto = archivo.getName();
             	
                 getAdmin().crearDato(archivo);
                 
                 getAdmin().generarBlockchain();
                 
                 getAdmin().mostrarBlockChain();
+                
+                
 			}else {
             	
                 texto = "El archivo no existe";
@@ -136,44 +113,7 @@ public class MainScreen extends JFrame
             
             System.out.println(ex.getMessage());
         }
-/*
-        
-        if (selectorArchivo.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
-        	
 
-        	
-        	
-        
-            archivo = selectorArchivo.getSelectedFile();
-            
-            texto.setText(archivo.getName());
-            
-            ruta = selectorArchivo.getSelectedFile().toString();
-            
-            try {
-            
-                if (archivo.exists()) {
-                    System.out.println(ruta);
-                	
-                    getAdmin().crearDato(archivo);
-                    
-                    getAdmin().generarBlockchain();
-                    
-                    getAdmin().mostrarBlockChain();
-                    
-                } else {
-                	
-                    texto.setText("El archivo no existe");
-                    
-                }
-                
-            } catch (Exception ex) {
-                
-                System.out.println(ex.getMessage());
-            }
-        }
-        
-        */
     }
 
 	public IAdministradorBlockchain getAdmin() {
