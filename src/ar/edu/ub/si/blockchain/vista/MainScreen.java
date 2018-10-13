8e6681fb4eb9a0d48910eaf90f1decf40c67c9b6
Fernando Romero <fer.romero1991@gmail.com>
2018-10-12 22:14:55 -0300

    se crea nueva interfaz con menu

--- src/ar/edu/ub/si/blockchain/vista/MainScreen.java
+++ src/ar/edu/ub/si/blockchain/vista/MainScreen.java
@@ -11,7 +11,7 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 import ar.edu.ub.si.blockchain.interfaces.IAdministradorBlockchain;
 
 
-public class MainScreen extends JFrame implements ActionListener
+public class MainScreen extends JFrame 
 {
 
 	private static final long serialVersionUID = 1L;
@@ -27,21 +27,31 @@ public class MainScreen extends JFrame implements ActionListener
 	public MainScreen(IAdministradorBlockchain admin) 
 	{
 		setAdmin(admin);
-		addElements();
-		setActionListeners();
-	
-		
+		this.startWindow();
+		this.addElements();
+		//setActionListeners();		
 	}
 
-	private void setActionListeners() 
-	{
-		boton.addActionListener(this);
-		
+	private void startWindow() {
+		this.setLocation(300, 300);
+		this.setSize(400, 400);
+		this.setResizable(false);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);	
 	}
+
 
 	private void addElements() 
 	{
 		
+		JMenuBar menuBar = new JMenuBar();
+		
+		
+		menuBar.add(this.createMenuFile());
+		menuBar.add(this.createMenuBd());
+		
+		this.setJMenuBar(menuBar);
+		
+		/*
 		jPanel = new JPanel();
 		
 		texto = new JLabel();
@@ -60,26 +70,80 @@ public class MainScreen extends JFrame implements ActionListener
 		jPanel.add(boton);
 		
 		boton.setText("Buscar Archivo");
-		
+		*/
 	}
 
-	@Override
-	public void actionPerformed(ActionEvent aEvent) {
+	private JMenu createMenuBd() {
+		JMenu menu = new JMenu("DataBase");
+		JMenuItem menuItem = new JMenuItem("Delete all records");
+		menuItem.addActionListener(this::onClickDatabaseDelete);
+		menu.add(menuItem);
 		
-		if(aEvent.getSource().equals(boton)) {
-			//JOptionPane.showMessageDialog(null,"BOTON APRETADO");
-			leerFichero();
-		}
+		return menu;
+	}
 
+	private JMenu createMenuFile() {
+		JMenu menu = new JMenu("File");
+		JMenuItem menuItem = new JMenuItem("Upload pdf");
+		menuItem.addActionListener(this::onClickMenuUpload);
+		menu.add(menuItem);
 		
+		menuItem = new JMenuItem("Exit");
+		menuItem.addActionListener(this::onClickMenuExit);
+		menu.add(menuItem);
+		
+		return menu;
+	}
+
+	public void onClickDatabaseDelete (ActionEvent e) {
+		admin.eliminarTodosLosRegistros();
+	}
+	public void onClickMenuUpload(ActionEvent e) {
+		leerFichero();
 	}
 	
-	public void leerFichero() {
+	public void onClickMenuExit(ActionEvent e) {
+		this.dispose();
+	}
+	
 
+	
+	public void leerFichero() {
 		
+		JFileChooser file = new JFileChooser();
+		file.showOpenDialog(this);
+		File archivo = file.getSelectedFile(); 
+		
+		String texto = archivo.getName();
+		
+		ruta = file.getSelectedFile().toString();
+		
+		try {
+			if(archivo.exists()) {
+                System.out.println(ruta);
+            	
+                getAdmin().crearDato(archivo);
+                
+                getAdmin().generarBlockchain();
+                
+                getAdmin().mostrarBlockChain();
+			}else {
+            	
+                texto = "El archivo no existe";
+                
+            }
+		}catch (Exception ex) {
+            
+            System.out.println(ex.getMessage());
+        }
+/*
         
         if (selectorArchivo.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
         	
+
+        	
+        	
+        
             archivo = selectorArchivo.getSelectedFile();
             
             texto.setText(archivo.getName());
@@ -108,6 +172,8 @@ public class MainScreen extends JFrame implements ActionListener
                 System.out.println(ex.getMessage());
             }
         }
+        
+        */
     }
 
 	public IAdministradorBlockchain getAdmin() {
