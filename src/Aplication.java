57d8165c4b7af38acbaaadfecd8cc0527d7acf96
fernandoromero91 <37422646+fernandoromero91@users.noreply.github.com>
2018-10-25 21:33:09 -0300

    se comenta en el main para probar de obtener el root del tree

--- src/Aplication.java
+++ src/Aplication.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import ar.edu.ub.si.blockchain.interfaces.IAdministradorBlockchain;
+import ar.edu.ub.si.blockchain.merkleTrees.MerkleTrees;
 import ar.edu.ub.si.blockchain.vista.AdminScreen;
 
 
@@ -9,16 +13,35 @@ public class Aplication {
 
 	public static void main(String[] args) {
 		
-		IAdministradorBlockchain admin = new AdministradorBlockchain();
+		/*
 		
-<<<<<<< HEAD
+		IAdministradorBlockchain admin = new AdministradorBlockchain();
+
 		
 		AdminScreen.generateScreen( admin);
-=======
+
 		//levantar un archivo conf para los properties
->>>>>>> master
+
 		
 		AdminScreen.generateScreen( admin );
+		*/
+		
+		
+		
+		
+		// PRUEBA DEL ROOT MERKLE TREE
+		
+		List<String> tempTxList = new ArrayList<String>();
+	    tempTxList.add("a");
+	    tempTxList.add("b");
+	    tempTxList.add("c");
+	    tempTxList.add("d");
+	    tempTxList.add("e");
+	    
+	    MerkleTrees merkleTrees = new MerkleTrees(tempTxList);
+	    merkleTrees.merkle_tree();
+	    System.out.println("root : " + merkleTrees.getRoot());
+	
 		
 	}
 	
