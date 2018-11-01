3bd3e8d45846cee27229c7963447294b66cfb9e4
Fernando Romero <fer.romero1991@gmail.com>
2018-11-01 10:02:12 -0300

    se limpia codigo

--- src/ar/edu/ub/si/blockchain/data/MerkleTree.java
+++ src/ar/edu/ub/si/blockchain/data/MerkleTree.java
@@ -19,7 +19,7 @@ public class MerkleTree implements IOperacionesHash {
 	   * constructor
 	   * @param txList transaction List
 	   */
-	  public MerkleTrees(List<String> txList) {
+	  public MerkleTree(List<String> txList) {
 	    this.txList = txList;
 	    root = "";
 	  }
@@ -61,15 +61,11 @@ public class MerkleTree implements IOperacionesHash {
 	      String right = "";
 	      if (index != tempTxList.size()) {
 	        right = tempTxList.get(index);
+	      }else {
+	    	  right = left;
 	      }
-/*
-	      // sha2 hex value
-	      String sha2HexValue = getSHA2HexValue(left + right);
-	      newTxList.add(sha2HexValue);
-	      index++;
-	      */
+
 	      // MD5
-	     // String mD5Value = generarHashMD5(left + right);
 	     String mD5Value = getMD5(left + right);
 	      newTxList.add(mD5Value);
 	      index++;
@@ -79,29 +75,7 @@ public class MerkleTree implements IOperacionesHash {
 	    return newTxList;
 	  }
 	  
-	  /**
-	   * Return hex string
-	   * @param str
-	   * @return
-	   */
-	  public String getSHA2HexValue(String str) {
-	        byte[] cipher_byte;
-	        try{
-	            MessageDigest md = MessageDigest.getInstance("SHA-256");
-	            md.update(str.getBytes());
-	            cipher_byte = md.digest();
-	            StringBuilder sb = new StringBuilder(2 * cipher_byte.length);
-	            for(byte b: cipher_byte) {
-	              sb.append(String.format("%02x", b&0xff) );
-	            }
-	            return sb.toString();
-	        } catch (Exception e) {
-	                e.printStackTrace();
-	        }
-	        
-	        return "";
-	  }
-	  
+
 	  /**
 	   * Get Root
 	   * @return
@@ -110,20 +84,6 @@ public class MerkleTree implements IOperacionesHash {
 	    return this.root;
 	  }
 
-	private String generarHashMD5(String str) {
-
-
-			   String md5 = null;
-			   try {
-			         MessageDigest mdEnc = MessageDigest.getInstance("MD5"); //Encryption algorithm
-			         mdEnc.update(str.getBytes(), 0, str.length());
-			         md5 = new BigInteger(1, mdEnc.digest()).toString(16); // Encrypted string
-			        } 
-			    catch (Exception ex) {
-			         return "";
-			    }
-			    return md5;
-			}
 	
 	private String getMD5(String input) {
 		 try {
@@ -150,8 +110,7 @@ public class MerkleTree implements IOperacionesHash {
 
 	@Override
 	public String getHash() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.root;
 	}
 
 }
